c5856eb6a3c8488766f532c9700d015fa507eda5
Juan Sensio <it@earthpulse.es>
2022-10-27 15:21:42 +0200

    update

---
+++ uis/eotdl/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Hero from "../components/layout/home/Hero.svelte";
+  import Cards from "../components/layout/home/Cards.svelte";
+  import Consortium from "../components/layout/home/Consortium.svelte";
+</script>
+
+<div class="w-full flex flex-col items-center">
+  <Hero />
+  <Cards />
+  <Consortium />
+</div>
