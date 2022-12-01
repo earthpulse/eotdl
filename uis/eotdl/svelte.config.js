efb666c8b158ffbca0fccdc22ee91912b2bde199
Juan Sensio <it@earthpulse.es>
2022-12-01 13:51:25 +0100

    vercel adapter

--- uis/eotdl/svelte.config.js
+++ uis/eotdl/svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+import vercel from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,7 @@ const config = {
   }),
 
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
   },
 };
 
