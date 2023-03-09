debce6f798b874c0e59a0fc46cee673c0832bb8f
Juan Sensio <it@earthpulse.es>
2023-03-09 15:39:45 +0100

    update api and cli auth

--- cli/eotdl_cli/main.py
+++ cli/eotdl_cli/main.py
@@ -1,78 +1,59 @@
 import typer
 import requests
 import time
-from auth0.authentication.token_verifier import TokenVerifier, AsymmetricSignatureVerifier
 import jwt
 import os 
 import json 
 from pathlib import Path
 
-AUTH0_DOMAIN = 'earthpulse.eu.auth0.com'
-AUTH0_CLIENT_ID = 'sC5WflzmPoj058FJYL2ckENutxJL4PTW'
 ALGORITHMS = ['RS256']
+API_URL = 'http://localhost:8000/'
+home = str(Path.home())
+creds_path = home + '/.etodl/creds.json'
 
 app = typer.Typer()
 
-def login():
-    home = str(Path.home())
-    creds_path = home + '/.etodl/creds.json'
-    # check if creds exist
+def is_logged():
     if os.path.exists(creds_path):
         with open(creds_path, 'r') as f:
             creds = json.load(f)
-        validate_token(creds['id_token'])
         current_user = jwt.decode(creds['id_token'], algorithms=ALGORITHMS, options={"verify_signature": False})    
         return current_user
-    # if not, retrieve them from auth0
-    device_code_payload = {
-        'client_id': AUTH0_CLIENT_ID,
-        'scope': 'openid profile email'
-    }
-    device_code_response = requests.post('https://{}/oauth/device/code'.format(AUTH0_DOMAIN), data=device_code_payload)
-    if device_code_response.status_code != 200:
-        print('Error generating the device code')
+    else:
+        return False
+
+def auth():
+    user = is_logged()
+    if user:
+        return user
+    response = requests.get(API_URL + 'auth/login')
+    if response.status_code != 200:
+        print('login error')
         raise typer.Exit(code=1)
-    print('Device code successful')
-    device_code_data = device_code_response.json()
-    print('1. On your computer or mobile device navigate to: ', device_code_data['verification_uri_complete'])
-    print('2. Enter the following code: ', device_code_data['user_code'])
-    token_payload = {
-        'grant_type': 'urn:ietf:params:oauth:grant-type:device_code',
-        'device_code': device_code_data['device_code'],
-        'client_id': AUTH0_CLIENT_ID
-    }
+    data = response.json()
+    print('On your computer or mobile device navigate to: ', data['login_url'])
     authenticated = False
-    while not authenticated:
-        print('Checking if the user completed the flow...')
-        token_response = requests.post('https://{}/oauth/token'.format(AUTH0_DOMAIN), data=token_payload)
-        token_data = token_response.json()
-        if token_response.status_code == 200:
+    t0 = time.time()
+    max_t = 30
+    while not authenticated and time.time() - t0 < max_t:
+        response = requests.get(API_URL + 'auth/token?code=' + data['code'])
+        token_data = response.json()
+        if response.status_code == 200:
             print('Authenticated!')
             print('- Id Token: {}...'.format(token_data['id_token'][:10]))
-            validate_token(token_data['id_token'])
-            # save token data in fileç
+            # save token data in file
+            os.makedirs(home + '/.etodl', exist_ok=True)
             with open(creds_path, 'w') as f:
                 json.dump(token_data, f)
+            print('Saved credentials to: ', creds_path)
             current_user = jwt.decode(token_data['id_token'], algorithms=ALGORITHMS, options={"verify_signature": False})
             authenticated = True
             return current_user
-        elif token_data['error'] not in ('authorization_pending', 'slow_down'):
-            print(token_data['error_description'])
-            raise typer.Exit(code=1)
         else:
-            time.sleep(device_code_data['interval'])
-
-def validate_token(id_token):
-    """
-    Verify the token and its precedence
-
-    :param id_token:
-    """
-    jwks_url = 'https://{}/.well-known/jwks.json'.format(AUTH0_DOMAIN)
-    issuer = 'https://{}/'.format(AUTH0_DOMAIN)
-    sv = AsymmetricSignatureVerifier(jwks_url)
-    tv = TokenVerifier(signature_verifier=sv, issuer=issuer, audience=AUTH0_CLIENT_ID)
-    tv.verify(id_token)
+            time.sleep(2)
+    if not authenticated:
+        print('Authentication timed out')
+        raise typer.Exit(code=1)
 
 @app.callback()
 def callback():
@@ -82,9 +63,27 @@ def callback():
 @app.command()
 def hello(name: str = 'World!'):
     """Say hello to someone"""
-    user = login()
-    print(user)
     typer.echo(f"hello, {name}")
+
+
+@app.command()
+def login():
+    user = auth()
+    typer.echo(f"You are logged in as {user['email']}")
+
+@app.command()
+def logout():
+    user = is_logged()
+    if user:
+        typer.echo(f"You are logged in as {user['email']}")
+        typer.confirm(f"Are you sure you want to logout?", abort=True)
+        os.remove(creds_path)
+        response = requests.get(API_URL + 'auth/logout')
+        logout_url = response.json()['logout_url']
+        typer.echo(f"You are logged out.")
+        typer.echo(f"If you want to login with a different account, visit {logout_url} and login again.")
+    else:
+        typer.echo(f"You are not logged in.")
 
 if __name__ == "__main__":
     app()
