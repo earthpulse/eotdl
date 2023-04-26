26fc660ce9d3f61989b6a70cc41280ddb03b41aa
Juan Sensio <it@earthpulse.es>
2023-04-26 18:10:07 +0200

    update ui

--- uis/eotdl/src/routes/+page.js
+++ uis/eotdl/src/routes/+page.js
@@ -1,17 +1,7 @@
-import retrieveDatasets from '../lib/datasets/retrieveDatasets'
-import retrieveDatasetsLeaderboard from '../lib/datasets/retrieveDatasetsLeaderboard'
-import retrievePopularDatasets from '../lib/datasets/retrievePopularDatasets'
-
 export const load = async ({fetch}) => {
-	const datasets = await retrieveDatasets(fetch, 3)
-	const leaderboard = await retrieveDatasetsLeaderboard(fetch)
-	const popular_datasets = await retrievePopularDatasets(fetch, 3)
 	const response = await fetch(`/api/posts`);
 	const posts = await response.json();
 	return {
-		leaderboard,
-		datasets,
-		popular_datasets,
 		posts
 	}
 }
