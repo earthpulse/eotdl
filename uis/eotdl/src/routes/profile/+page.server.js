afccaf74a28d4b365e779abbdd7821b1be93a1d6
Juan Sensio <it@earthpulse.es>
2023-05-08 17:17:00 +0200

    fix bug username in profile

--- uis/eotdl/src/routes/profile/+page.server.js
+++ uis/eotdl/src/routes/profile/+page.server.js
@@ -1,9 +1,14 @@
 import { redirect } from '@sveltejs/kit';
+import retrieveUserData from '../../lib/auth/retrieveUserData';
 
-export async function load({ locals }) {
+export async function load({ locals, fetch }) {
     if (!locals?.user) 
         throw redirect(307, '/api/auth/login');
-    return locals
+    const userData = await retrieveUserData(locals.id_token, fetch)
+    return {
+        id_token: locals.id_token,
+        user: userData
+    }
 }
 
 export const prerender = false;
