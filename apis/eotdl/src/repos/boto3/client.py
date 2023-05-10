0fec7dd7ac1173094e299d8aa5effbb6321ae9ad
Juan Sensio <it@earthpulse.es>
2023-05-10 14:22:47 +0200

    update

--- apis/eotdl/src/repos/boto3/client.py
+++ apis/eotdl/src/repos/boto3/client.py
@@ -22,10 +22,12 @@ def get_client():
         "s3",
         aws_access_key_id=os.environ["ACCESS_KEY_ID"],
         aws_secret_access_key=os.environ["SECRET_ACCESS_KEY"],
-        # region_name=os.getenv("S3_REGION", "us-east-1"),
+        region_name=os.getenv("S3_REGION", "us-east-1"),
         # use_ssl=os.getenv("S3_SSL", False),
         endpoint_url=HTTP_PREFIX + os.environ["S3_ENDPOINT"],
         # verify=False,
-        # config=Config(signature_version="s3v4", s3={"addressing_style": "path"}),
-        config=boto3.session.Config(signature_version="s3v4"),
+        config=boto3.session.Config(
+            signature_version="s3v4", s3={"addressing_style": "path"}
+        ),
+        # config=boto3.session.Config(signature_version="s3v4"),
     )
