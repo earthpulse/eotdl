270433ee4870b634aea9e9543addfbd2bc704682
Juan Sensio <it@earthpulse.es>
2023-05-11 19:10:33 +0200

    improve upload and download of large datasets

--- apis/eotdl/src/repos/minio/MinioRepo.py
+++ apis/eotdl/src/repos/minio/MinioRepo.py
@@ -49,7 +49,7 @@ class MinioRepo:
 
     async def data_stream(self, id):
         with self.client.get_object(self.bucket, self.get_object(id)) as stream:
-            for chunk in stream.stream(1024 * 1024 * 100):  # Stream in chunks of 100MB
+            for chunk in stream.stream(1024 * 1024 * 10):  # Stream in chunks of 10MB
                 yield chunk
 
     def object_info(self, id):
