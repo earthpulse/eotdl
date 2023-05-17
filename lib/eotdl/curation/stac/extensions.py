feb71935ec4a2ad301a3bd9639139ee75f4a1128
Fran Martin <fran@earthpulse.ai>
2023-05-17 12:16:17 +0200

    feat(use-case-b): changed STAC generation approach to match demos

--- lib/eotdl/curation/stac/extensions.py
+++ lib/eotdl/curation/stac/extensions.py
@@ -50,7 +50,21 @@ class EOS2ExtensionObject(STACExtensionObject):
 
     def __init__(self) -> None:
         super().__init__()
-        pass
+        self.bands = [
+            Band.create(name='Aerosols', description='Coastal aerosol, 442.7 nm (S2A), 442.3 nm (S2B)', common_name='coastal'),
+            Band.create(name='Blue', description='Blue, 492.4 nm (S2A), 492.1 nm (S2B)', common_name='blue'),
+            Band.create(name='Green', description='Green, 559.8 nm (S2A), 559.0 nm (S2B)', common_name='green'),
+            Band.create(name='Red', description='Red, 664.6 nm (S2A), 665.0 nm (S2B)', common_name='red'),
+            Band.create(name='Red edge 1', description='Vegetation red edge, 704.1 nm (S2A), 703.8 nm (S2B)', common_name='rededge'),
+            Band.create(name='Red edge 2', description='Vegetation red edge, 740.5 nm (S2A), 739.1 nm (S2B)', common_name='rededge'),
+            Band.create(name='Red edge 3', description='Vegetation red edge, 782.8 nm (S2A), 779.7 nm (S2B)', common_name='rededge'),
+            Band.create(name='NIR', description='NIR, 832.8 nm (S2A), 833.0 nm (S2B)', common_name='nir'),
+            Band.create(name='Red edge 4', description='Narrow NIR, 864.7 nm (S2A), 864.0 nm (S2B)', common_name='nir08'),
+            Band.create(name='Water vapour', description='Water vapour, 945.1 nm (S2A), 943.2 nm (S2B)', common_name='nir09'),
+            Band.create(name='Cirrus', description='SWIR – Cirrus, 1373.5 nm (S2A), 1376.9 nm (S2B)', common_name='cirrus'),
+            Band.create(name='SWIR1', description='SWIR, 1613.7 nm (S2A), 1610.4 nm (S2B)', common_name='swir16'),
+            Band.create(name='SWIR2', description='SWIR, 2202.4 nm (S2A), 2185.7 nm (S2B)', common_name='swir22'),
+        ]
 
     def add_extension_to_object(self, obj: pystac.Item|pystac.Asset) -> pystac.Item|pystac.Asset:
         """
@@ -63,18 +77,12 @@ class EOS2ExtensionObject(STACExtensionObject):
         # Add EO extension
         eo_ext = EOExtension.ext(obj, add_if_missing=True)
         # Add the existing bands from the rasters assets list
-        bands = list()
-        for raster in self.rasters_assets:
-            raster_name = raster.split('/')[-1]
-            band_name = raster_name.split('.')[0]
-            band = self.sentinel_2_bands_dict[band_name]
-            bands.append(band)
-        eo_ext.apply(bands=bands)
+        eo_ext.apply(bands=self.bands)
         # Add common metadata
         obj.common_metadata.constellation = "Sentinel-2"
         obj.common_metadata.platform = "Sentinel-2"
         obj.common_metadata.instruments = ["Sentinel-2"]
-        obj.common_metadata.gsd = 10   # Where to obtain it?
+        obj.common_metadata.gsd = 10   # TODO Where to obtain it?
 
         return obj
 
@@ -90,6 +98,6 @@ class DEMExtensionObject(STACExtensionObject):
 
 type_stac_extensions_dict = {
     'sar': SarExtensionObject(),
-    'eo-s2': EOS2ExtensionObject(),
+    'eo': EOS2ExtensionObject(),
     'dem': DEMExtensionObject()
 }
