371055e558554344068c0a4025b398c0e211fb9b
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2023-05-18 17:14:34 +0200

    update:: search working // can fetch dates in array

--- usecases/usecaseA/scripts/spot_search.sh
+++ usecases/usecaseA/scripts/spot_search.sh
@@ -4,71 +4,89 @@
 # 
 
 URL="https://services.sentinel-hub.com/api/v1/dataimport/search";
-
-BBOX=$1;
-DATE1=$2;
-DATE2=$3;
-
-
 get_token() {
-
-
-	curl -X POST\
+	curl -s -X POST\
 		--url https://services.sentinel-hub.com/oauth/token\
 		-H "content-type: application/x-www-form-urlencoded"\
 		-d "grant_type=client_credentials&client_id=$(sed '1q;d' credentials)"\
 		--data-urlencode "client_secret=$(sed '2q;d' credentials)"
-
 }
 
+end=$(date +%s)
 
-QUERY=(
+if [[ -z $SHTOKEN ]] || [[ $((end-SHTOKENSTART)) -ge $SHTOKENEXPIRESIN ]];
+then
+	token_response=$(get_token);
+	export SHTOKEN=$(echo $token_response | grep -oP "(?<=\"access_token\":\").*(?=\",)");
+	export SHTOKENEXPIRESIN=$(echo $token_response | grep -oP "(?<=\"expires_in\":)\d*");
+	export SHTOKENSTART=$(date +%s);
+fi
 
-	"{
-	
-	'provider': 'AIRBUS',
-	'bounds': {
+request () {
 
-		'BBox': {
+	QUERY=("{
 		
-			'bbox': $BBOX,
-			'crs': 4326
-			
-			}
-
-		},
-	'data': [
-		{
-		'constellation': 'SPOT',
-		'dataFilter': {
-			'timeRange': {
-				'from': $DATE1,
-				'to': $DATE2
+		\"provider\": \"AIRBUS\",
+		\"bounds\": {
+			\"properties\": { \"crs\": \"http://www.opengis.net/def/crs/EPSG/0/4326\" },
+			\"bbox\": $1
+			},
+		\"data\": [
+			{
+			\"constellation\": \"SPOT\",
+			\"dataFilter\": {
+				\"timeRange\": {
+					\"from\": \"$2\",
+					\"to\": \"$3\"
+					}
 				}
 			}
-		}
-	]
-		
-	}"
-
-)
-
-
-echo $QUERY;
-
-token_response=$(get_token)
-token=$(echo $token_response | grep -oP "(?<=\"access_token\":\").*(?=\",)");
-time=$(echo $token_response | grep -oP "(?<=\"expires_in\":)\d*");
-start=$(date +%s);
-
-sleep 2
+		]
+			
+		}")
+	
+	curl -s -X POST\
+		-H 'accept: application/json'\
+      		-H "Content-Type: application/json"\
+ 		-H "Authorization: Bearer $SHTOKEN"\
+ 		-d "$QUERY" "$URL";
+	}
 
 end=$(date +%s);
 
-echo $token $time $((end-start))
+while IFS= read -r row;
+do
+	datestr=${row%%;*};
+	bboxstr=${row##*;};
+	from=$(date --date "$datestr -2 days" +%Y-%m-%dT00:00:00Z);
+	to=$(date --date "$datestr +2 days" +%Y-%m-%dT00:00:00Z);
+	
+	# response=$(request "$bboxstr" $from $to)
+	response=$(request "$2" $from $to)
 
-# curl -X POST -H 'accept: application/json'\
-#      	-H "Content-Type: application/json"\
-# 	-H "authorization: Bearer $token"\
-# 	-d "$QUERY" "$URL";
+	totalResults="totalResults: $(echo $response | grep -oP "(?<=\"totalResults\":)\d*")"
+	ids=$(echo $response | grep -oP nothing)
+	
+	echo $response |
+	grep -oP "(?<=\"acquisitionDate\":)\"\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\dZ\"" |
+	sed s/\"//g |
+	read -a dates
+	
+	echo $response
+	echo $totalResults
+	echo ${dates[@]} ${dates[0]} ${dates[1]} ${#dates[@]}
+
+	end=$(date +%s);
+	if [[ $((end-SHTOKENSTART)) -ge $SHTOKENEXPIRESIN ]]
+	then
+		token_response=$(get_token)
+		export SHTOKEN=$(echo $token_response | grep -oP "(?<=\"access_token\":\").*(?=\",)");
+		export SHTOKENEXPIRESIN=$(echo $token_response | grep -oP "(?<=\"expires_in\":)\d*");
+		export SHTOKENSTART=$(date +%s);
+	fi
+
+	break;
+
+done < $1;
+
 
