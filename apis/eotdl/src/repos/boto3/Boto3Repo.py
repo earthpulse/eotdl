47903aa305db58a299fbd5e27374ae3761be3078
Juan Sensio <it@earthpulse.es>
2023-06-13 09:42:36 +0200

    update

--- apis/eotdl/src/repos/boto3/Boto3Repo.py
+++ apis/eotdl/src/repos/boto3/Boto3Repo.py
@@ -24,11 +24,14 @@ class Boto3Repo:
         )
 
     def complete_multipart_upload(self, storage, upload_id):
+        # BUG ??? not all parts ??? local ok, cloud small files ok, cloud large files (>50GB) ko :(
+        # TODO: check parts
         parts = self.client.list_parts(
             Bucket=self.bucket,
             Key=storage,
             UploadId=upload_id,
         )["Parts"]
+        # maybe not needed and causing bug???
         sorted_parts = sorted(parts, key=lambda part: part["PartNumber"])
         etags = [
             {"PartNumber": part["PartNumber"], "ETag": part["ETag"]}
@@ -40,3 +43,21 @@ class Boto3Repo:
             MultipartUpload={"Parts": etags},
             UploadId=upload_id,
         )
+
+        # response = self.client.complete_multipart_upload(
+        #     Bucket=self.bucket,
+        #     Key=storage,
+        #     MultipartUpload={"Parts": etags},
+        #     UploadId=upload_id,
+        # )
+        # response_etags = [part["ETag"] for part in response["Parts"]]
+        # parts_etags = [part["ETag"] for part in parts]
+        # assert response_etags == parts_etags
+
+        # Try
+        # for part_number in range(1, num_parts + 1):
+        #     parts.append({'PartNumber': part_number})
+        # response = s3.complete_multipart_upload(Bucket='your-bucket',
+        #                                         Key='your-object-key',
+        #                                         UploadId=upload_id,
+        #                                         MultipartUpload={'Parts': parts})
