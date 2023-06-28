b2ae2bbfdace772fafaf133d0c1669d37b3419fb
Juan Sensio <it@earthpulse.es>
2023-06-28 10:35:47 +0200

    q1

--- eotdl/eotdl/datasets/__init__.py
+++ eotdl/eotdl/datasets/__init__.py
@@ -1,3 +1,3 @@
-from .ingest import ingest_file, ingest_folder
+from .ingest import ingest_file, ingest_folder, ingest_q1
 from .download import download_dataset
 from .retrieve import retrieve_datasets, retrieve_dataset, list_datasets
