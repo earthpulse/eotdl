5d6dfc2a5c51da9e36ad878e6fa25162e4d48d09
Juan Sensio <it@earthpulse.es>
2023-07-18 18:02:23 +0200

    migrate db

--- apis/eotdl/routers/migrate.py
+++ apis/eotdl/routers/migrate.py
@@ -1,7 +1,5 @@
-from fastapi.exceptions import HTTPException
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, Depends
 import logging
-import uuid
 import os
 from minio.commonconfig import CopySource
 import hashlib
@@ -20,62 +18,72 @@ bucket = os.environ.get("S3_BUCKET")
 @router.get("", include_in_schema=False)
 def migrate_db(isAdmin: bool = Depends(key_auth)):
     db = get_db()
+    collections = db.list_collection_names()
     s3 = get_client()
     # create a backup of the changed collections
-    random_id = uuid.uuid4()
-    collection_name = "users-" + str(random_id)
-    db[collection_name].insert_many(db["users"].find())
+    collection_name = "users-bck"
+    if not collection_name in collections:
+        db[collection_name].insert_many(db["users"].find())
     # update users
     #   - rename `model_count` to `models_count`
     for user in db["users"].find():
-        db["users"].update_one(
-            {"_id": user["_id"]},
-            {"$rename": {"model_count": "models_count"}},
-        )
-    collection_name = "datasets-" + str(random_id)
-    db[collection_name].insert_many(db["datasets"].find())
+        if "model_count" in user:
+            db["users"].update_one(
+                {"_id": user["_id"]},
+                {"$rename": {"model_count": "models_count"}},
+            )
+    collection_name = "datasets-bck"
+    if not collection_name in collections:
+        db[collection_name].insert_many(db["datasets"].find())
     # update datasets
     #   - rename `author` to `autors` and convert to list
     #   - rename `link` to `source`
     #   - remove `checksum`
     #   - add `files` as a list with one value
     for dataset in db["datasets"].find():
-        author = dataset["author"]
+        print("updating dataset", dataset["name"])
         name = dataset["name"]
         dataset_id = dataset["id"]
         size = dataset["size"]
-        db["datasets"].update_one(
-            {"_id": dataset["_id"]},
-            {"$rename": {"author": "authors", "link": "source"}},
-        )
-        db["datasets"].update_one(
-            {"_id": dataset["_id"]},
-            {"$set": {"authors": [author]}},
-        )
-        db["datasets"].update_one(
-            {"_id": dataset["_id"]},
-            {"$unset": {"checksum": ""}},
-        )
-        new_object_name = f"{dataset_id}/{name}.zip"
-        s3.copy_object(bucket, new_object_name, CopySource(bucket, f"{dataset_id}.zip"))
-        # compute new sha1 checksum
-        sha1_hash = hashlib.sha1()
-        with s3.get_object(bucket, new_object_name) as stream:
-            for chunk in stream.stream(1024 * 1024 * 10):
-                sha1_hash.update(chunk)
-            checksum = sha1_hash.hexdigest()
-        db["datasets"].update_one(
-            {"_id": dataset["_id"]},
-            {
-                "$set": {
-                    "files": [
-                        {
-                            "name": name + ".zip",
-                            "size": size,
-                            "checksum": checksum,
-                        }
-                    ]
-                }
-            },
-        )
+        if "author" in dataset:
+            author = dataset["author"]
+            db["datasets"].update_one(
+                {"_id": dataset["_id"]},
+                {"$rename": {"author": "authors", "link": "source"}},
+            )
+            db["datasets"].update_one(
+                {"_id": dataset["_id"]},
+                {"$set": {"authors": [author]}},
+            )
+        if "checksum" in dataset:
+            db["datasets"].update_one(
+                {"_id": dataset["_id"]},
+                {"$unset": {"checksum": ""}},
+            )
+        if not "files" in dataset:
+            new_object_name = f"{dataset_id}/{name}.zip"
+            s3.copy_object(
+                bucket, new_object_name, CopySource(bucket, f"{dataset_id}.zip")
+            )
+            # compute new sha1 checksum
+            sha1_hash = hashlib.sha1()
+            with s3.get_object(bucket, new_object_name) as stream:
+                for chunk in stream.stream(1024 * 1024 * 10):
+                    sha1_hash.update(chunk)
+                checksum = sha1_hash.hexdigest()
+            # add files list
+            db["datasets"].update_one(
+                {"_id": dataset["_id"]},
+                {
+                    "$set": {
+                        "files": [
+                            {
+                                "name": name + ".zip",
+                                "size": size,
+                                "checksum": checksum,
+                            }
+                        ]
+                    }
+                },
+            )
     return "Done"
