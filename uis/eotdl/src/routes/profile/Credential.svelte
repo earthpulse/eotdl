42eaaf9553e98d5d6214bd29634595d559a0be4c
Juan Sensio <it@earthpulse.es>
2023-07-26 19:13:26 +0200

    eox credentials generation and retrieva

---
+++ uis/eotdl/src/routes/profile/Credential.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let value = "lkjansfo9pansws34nfqp80w383";
+
+	const copyToClipboard = (text) => {
+		navigator.clipboard.writeText(value);
+		console.log("copied to clipboard");
+	};
+
+	const format = (text) => {
+		return text.slice(0, 5) + "****";
+	};
+</script>
+
+<button on:click={copyToClipboard}>{format(value)}</button>
---
+++ uis/eotdl/src/routes/profile/Credentials.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { user, id_token } from "$stores/auth";
+	import Credential from "./Credential.svelte";
+
+	const credentials = [
+		"GEODB_API_SERVER_PORT",
+		"GEODB_API_SERVER_URL",
+		"GEODB_AUTH_AUD",
+		"GEODB_AUTH_CLIENT_ID",
+		"GEODB_AUTH_CLIENT_SECRET",
+		"GEODB_AUTH_DOMAIN",
+		"SH_CLIENT_ID",
+		"SH_CLIENT_SECRET",
+		"SH_INSTANCE_ID",
+		"SH_OWNER_ID",
+	];
+</script>
+
+<h2 class="font-bold">Your credentials:</h2>
+<p class="text-red-400">*placeholder data</p>
+<button class="btn btn-outline w-[100px]">Download</button>
+{#each credentials as credential}
+	<span class="flex flex-row gap-2">
+		<p>{credential}:</p>
+		<Credential />
+	</span>
+{/each}
