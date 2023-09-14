56ad1497c4ef0240dd1c60fc51109f4b8c7dbf20
Juan Sensio <it@earthpulse.es>
2023-09-14 14:45:32 +0200

    update

--- apis/eotdl/routers/admin.py
+++ apis/eotdl/routers/admin.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, Depends
 from fastapi.exceptions import HTTPException
 import logging
+import os
 
 from .auth import key_auth
 
@@ -18,6 +19,15 @@ def logs(isAdmin: bool = Depends(key_auth), tail: int = 10):
         with open('/tmp/eotdl-api.log', 'r') as f:
             lines = f.readlines()
             return lines[-tail:]            
+    except Exception as e:
+        logger.exception('logs')
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=str(e))
+    
+@router.get("/env", include_in_schema=False)
+def logs(isAdmin: bool = Depends(key_auth)):
+    try:
+        return os.environ
     except Exception as e:
         logger.exception('logs')
         raise HTTPException(
