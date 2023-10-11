7bb4ceaf58f988c20b258a1b4b2392c8d28e38ce
Juan Sensio <it@earthpulse.es>
2023-10-11 17:09:08 +0200

    refactor

--- apis/eotdl/main.py
+++ apis/eotdl/main.py
@@ -58,7 +58,7 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(name)s %(message)s",
 )
 
-VERSION = "2023.10.16"
+VERSION = "2023.10.17"
 
 
 @app.get("/", name="home", include_in_schema=False)
