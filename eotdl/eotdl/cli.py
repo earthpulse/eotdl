4660d3f0a3b2fd2737254413464b8996bfa427e7
Juan Sensio <it@earthpulse.es>
2023-10-24 14:34:05 +0200

    update bids tutorials

--- eotdl/eotdl/cli.py
+++ eotdl/eotdl/cli.py
@@ -1,10 +1,17 @@
 import typer
 from .commands import auth, datasets
+from . import __version__
 
 app = typer.Typer()
 
 app.add_typer(auth.app, name="auth")
 app.add_typer(datasets.app, name="datasets")
+
+
+@app.command()
+def version():
+    typer.echo(f"EOTDL Version: {__version__}")
+
 
 if __name__ == "__main__":
     app()
