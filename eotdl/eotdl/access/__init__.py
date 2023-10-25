99f5502016692549a0c85d1678f56336db66fa37
Fran Martin <fran@earthpulse.ai>
2023-10-25 14:03:07 +0200

    Refactor notebooks

--- eotdl/eotdl/access/__init__.py
+++ eotdl/eotdl/access/__init__.py
@@ -1,3 +1,4 @@
 from .download import (download_sentinel_imagery,
                        search_and_download_sentinel_imagery)
 from .search import search_sentinel_imagery
+from .sentinelhub.parameters import SUPPORTED_SENSORS
