6742c06c18cb3f7fd54b0be6b645ef61ed8e58b5
Juan Sensio <it@earthpulse.es>
2023-10-26 15:48:18 +0200

    update project structure

--- eotdl/eotdl/cli.py
+++ eotdl/eotdl/cli.py
@@ -1,11 +1,12 @@
 import typer
-from .commands import auth, datasets
+from .commands import auth, datasets, models
 from . import __version__
 
 app = typer.Typer()
 
 app.add_typer(auth.app, name="auth")
 app.add_typer(datasets.app, name="datasets")
+app.add_typer(models.app, name="models")
 
 
 @app.command()
