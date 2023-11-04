80c7de47253fad4fc0ec95b44ae6a1c9dd7342da
Juan Sensio <it@earthpulse.es>
2023-11-04 14:56:04 +0100

    update

--- api/api/src/usecases/user/accept_user_terms_and_conditions.py
+++ api/api/src/usecases/user/accept_user_terms_and_conditions.py
@@ -5,6 +5,7 @@ from ...errors import UserDoesNotExistError
 from ...repos import UserDBRepo, EOXRepo
 from .retrieve_user import retrieve_user
 
+
 def accept_user_terms_and_conditions(user):
     repo, eox_repo = UserDBRepo(), EOXRepo()
     # check user exists
@@ -12,13 +13,12 @@ def accept_user_terms_and_conditions(user):
     # generate credentials
     errors = eox_repo.generate_credentials(user.uid, user.email)
     print(errors)
-    if len(errors) > 0:
-        raise Exception(errors)
+    # For some reason this can return error even if it works, I prefer to accept and if it doesn't work support users
+    # if len(errors) > 0:
+    #     raise Exception(errors)
     # update user
     data.update(
-        terms=TermsAndConditions(
-            geodb=True, sentinelhub=True, eotdl=True, eoxhub=True
-        ),
+        terms=TermsAndConditions(geodb=True, sentinelhub=True, eotdl=True, eoxhub=True),
         updatedAt=datetime.now(),
     )
     user = User(**data)
