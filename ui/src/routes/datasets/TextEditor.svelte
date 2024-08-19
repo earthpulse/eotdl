07c112abb7c764bdb8a21635f4896018ede3b3b5
Fernando Hernandez <ferndohm99emf@gmail.com>
2024-08-19 11:01:09 +0200

    fixed responsive freatures

--- ui/src/routes/datasets/TextEditor.svelte
+++ ui/src/routes/datasets/TextEditor.svelte
@@ -3,6 +3,21 @@
 	import 'carta-md/default.css'; /* Default theme */
 	import "$styles/carta-md.css"
   import DOMPurify from 'isomorphic-dompurify';
+  
+  var x = window.matchMedia("(max-width: 640px)")
+  let sm = x.matches ? true : false;
+  let normal = x.matches ? false : true;;
+  // Attach listener function on state changes
+  x.addEventListener("change", function() {
+    if(x.matches){
+      sm = true;
+      normal = false;
+    }
+    else{
+      sm = false;
+      normal = true;
+    }
+  });
 
   import TurndownService from 'turndown';
   const carta = new Carta({
@@ -20,7 +35,7 @@
 
 <div class="flex justify-center">
     <div on:change={renderHtml} class="w-[62rem] flex flex-col items-center justify-center p-2 rounded-xl">
-        <MarkdownEditor mode="split" {carta} bind:value />
+        <MarkdownEditor mode={sm ? "tabs" : "split"} {carta} bind:value />
     </div>
 </div>
 <slot />
