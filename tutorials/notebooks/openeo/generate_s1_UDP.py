f5049ae00655e9fa0a7e68a0481cf6862df65b5f
Hans Vanrompay <hansvanrompay@hotmail.com>
2024-11-18 19:40:59 +0100

    direct feature collection

--- tutorials/notebooks/openeo/generate_s1_UDP.py
+++ tutorials/notebooks/openeo/generate_s1_UDP.py
@@ -8,36 +8,26 @@ import geopandas as gpd
 from utils import default_geojson,compute_percentiles
 from s3proxy_utils import upload_geoparquet_file
 
-
-
-# Extract the GeoJSON from the input parameter or use the default if not provided
-
+["VH", "VV"]
+[0.1, 0.25, 0.50, 0.75, 0.9]
 ##Define input parameters
 temporal_extent = Parameter.temporal_interval(name="temporal_extent")
 area = Parameter.geojson(name="geometry", default = default_geojson)
 
-schema = {"type": "string", "enum":["VH", "VV"]}
-bands_param = Parameter.array(name="bands",description="Sentinel-1 bands to include in the composite.", item_schema=schema, default=["VH", "VV"], optional=True)
-percentile_param = Parameter.array(name="percentiles",description="percentiles calculated across the BAP composite.", default=[0.1, 0.25, 0.50, 0.75, 0.9], optional=True)
-
-# Get the values provided or fall back on the default
-user_bands = getattr(bands_param, 'value', bands_param.default)
-user_percentile = getattr(percentile_param, 'value', percentile_param.default)
-user_area = getattr(area, 'value', default_geojson)  
-
 #Define input scl
-connection=openeo.connect("openeofed.dataspace.copernicus.eu").authenticate_oidc()
+connection=openeo.connect("openeo.dataspace.copernicus.eu").authenticate_oidc()
 
 #upload the feature collection to the S3 proxy in order to use it within filter spatial
-feature_crs  = user_area.get("features", {})[0].get("properties", {})['crs']
-features = gpd.GeoDataFrame.from_features(user_area).set_crs(feature_crs)
-url = upload_geoparquet_file(features,connection)
+#feature_crs  = area.get("features", {})[0].get("properties", {})['crs']
+#features = gpd.GeoDataFrame.from_features(area).set_crs(feature_crs)
+#url = upload_geoparquet_file(area,connection)
+#area = connection.load_url(url, format="Parquet")
 
 # load collection
 sentinel1 = connection.load_collection(
     collection_id="SENTINEL1_GRD",
     temporal_extent=temporal_extent,
-    bands=user_bands).filter_spatial(connection.load_url(url, format="Parquet"))
+    bands=["VH", "VV"]).filter_spatial(area)
 
 # apply back scatter filtering
 sentinel1 = sentinel1.sar_backscatter(elevation_model= "COPERNICUS_30", coefficient="sigma0-ellipsoid")
@@ -45,7 +35,8 @@ sentinel1 = sentinel1.sar_backscatter(elevation_model= "COPERNICUS_30", coeffici
 #get montly composites
 composite = sentinel1.aggregate_temporal_period(period="week", reducer="mean")
 
-statistics = compute_percentiles(composite, user_percentile)
+statistics = compute_percentiles(composite, [0.1, 0.25, 0.50, 0.75, 0.9]
+)
 
 result = statistics.save_result(format="NetCDF")
 
@@ -54,13 +45,13 @@ process_id = "s1_weekly_statistics"
 connection.save_user_defined_process(
     user_defined_process_id=process_id,
     process_graph=result,
-    parameters=[temporal_extent, area, bands_param, percentile_param],
+    parameters=[temporal_extent, area],
 )
 
 spec = build_process_dict(
     process_id="s1_weekly_statistics",
     process_graph=result,
-    parameters=[temporal_extent, area, bands_param, percentile_param],
+    parameters=[temporal_extent, area],
 )
 
 with open("s1_weekly_statistics.json", "w") as f:
