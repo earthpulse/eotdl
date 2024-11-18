f424a6e292af8843851932119a83ca4a56ba9e06
Hans Vanrompay <hansvanrompay@hotmail.com>
2024-11-18 19:40:59 +0100

    direct feature collection

--- tutorials/notebooks/openeo/generate_s2_UDP.py
+++ tutorials/notebooks/openeo/generate_s2_UDP.py
@@ -13,35 +13,23 @@ from s3proxy_utils import upload_geoparquet_file
 temporal_extent = Parameter.temporal_interval(name="temporal_extent")
 area = Parameter.geojson(name="geometry")
 
-schema = {"type": "string", "enum":["B02","B03","B04","B05","B06","B07","B08","B8A","B11","B12"]}
-bands_param = Parameter.array(name="bands",description="Sentinel-2 L2A bands to include in the composite.", item_schema=schema, default=["B04", "B03", "B02"], optional=True)
-
-percentile_param = Parameter.array(name="percentiles",description="percentiles calculated across the BAP composite.", default=[0.1, 0.25, 0.50, 0.75, 0.9], optional=True)
-
-max_cloud_param = Parameter.integer(name="max_cloud_cover",description="Maximal cloud cover (%) used when loading in the data.", default=70, optional=True)
-
-
-# Get the values provided or fall back on the default
-user_bands = getattr(bands_param, 'value', bands_param.default)
-user_percentile = getattr(percentile_param, 'value', percentile_param.default)
-user_max_cloud = getattr(max_cloud_param, 'value', max_cloud_param.default)
-user_area = getattr(area, 'value', default_geojson)  
-
 #Define input scl
-connection=openeo.connect("openeofed.dataspace.copernicus.eu").authenticate_oidc()
+connection=openeo.connect("openeo.dataspace.copernicus.eu").authenticate_oidc()
 
 #upload the feature collection to the S3 proxy in order to use it within filter spatial
-feature_crs  = user_area.get("features", {})[0].get("properties", {})['crs']
-features = gpd.GeoDataFrame.from_features(user_area).set_crs(feature_crs)
-url = upload_geoparquet_file(features,connection)
+#feature_crs  = area.get("features", {})[0].get("properties", {})['crs']
+#features = gpd.GeoDataFrame.from_features(area).set_crs(feature_crs)
+#url = upload_geoparquet_file(features,connection)
+#url = upload_geoparquet_file(area,connection)
+#area = connection.load_url(url, format="Parquet")
 
 
 scl = connection.load_collection(
     "SENTINEL2_L2A",
     temporal_extent=temporal_extent,
     bands=["SCL"],
-    max_cloud_cover=user_max_cloud
-).filter_spatial(connection.load_url(url, format="Parquet"))
+    max_cloud_cover=70
+).filter_spatial(area)
 
 
 mask = scl.process("to_scl_dilation_mask", data=scl) 
@@ -50,16 +38,16 @@ mask = scl.process("to_scl_dilation_mask", data=scl)
 sentinel2 = connection.load_collection(
     "SENTINEL2_L2A",
     temporal_extent = temporal_extent,
-    bands = user_bands,
-    max_cloud_cover=user_max_cloud
-).filter_spatial(connection.load_url(url, format="Parquet"))
+    bands = ["B02", "B03", "B04", "B08", "B11", "B12"],
+    max_cloud_cover=70
+).filter_spatial(area)
 
 sentinel2_masked = sentinel2.mask(mask)
 
 #select the first day per month
 composite = sentinel2_masked.aggregate_temporal_period(period="week", reducer="mean")
 
-statistics = compute_percentiles(composite, user_percentile)
+statistics = compute_percentiles(composite, [0.1, 0.25, 0.50, 0.75, 0.9])
 
 result = statistics.save_result(format="NetCDF")
 
@@ -68,13 +56,13 @@ process_id = "s2_bap_statistics"
 connection.save_user_defined_process(
     user_defined_process_id=process_id,
     process_graph=result,
-    parameters=[temporal_extent, area, bands_param, percentile_param],
+    parameters=[temporal_extent, area],
 )
 
 spec = build_process_dict(
     process_id="s2_weekly_statistics",
     process_graph=result,
-    parameters=[temporal_extent, area, bands_param, percentile_param],
+    parameters=[temporal_extent, area],
 )
 
 with open("s2_weekly_statistics.json", "w") as f:
