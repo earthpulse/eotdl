60eb63e630faeff5f62a19e0d532a04f7dc11fd0
Juan Sensio <it@earthpulse.es>
2025-01-24 13:21:02 +0100

    add training UI

--- ui/src/routes/datasets/[name]/Train.svelte
+++ ui/src/routes/datasets/[name]/Train.svelte
@@ -1,5 +1,9 @@
 <script>
 	export let dataset;
+
+	// training parameters
+	let batch_size = 16;
+	let epochs = 10;
 </script>
 
 <label for="trainining-template" class="btn btn-ghost btn-outline">Train</label>
@@ -18,6 +22,31 @@
 				target="_blank">Notebook</a
 			>
 			<p>or execute a headless training job:</p>
+			<div class="flex flex-col gap-2 border-2 border-slate-200 p-2">
+				<h2 class="font-bold">Parameters:</h2>
+				<div class="flex justify-between items-center">
+					<label>Dataset name:</label>
+					<span class="w-32 text-right">{dataset.name}</span>
+				</div>
+				<div class="flex justify-between items-center">
+					<label for="batch_size">Batch size:</label>
+					<input
+						type="number"
+						id="batch_size"
+						bind:value={batch_size}
+						class="input input-bordered input-xs w-24"
+					/>
+				</div>
+				<div class="flex justify-between items-center">
+					<label for="epochs">Epochs:</label>
+					<input
+						type="number"
+						id="epochs"
+						bind:value={epochs}
+						class="input input-bordered input-xs w-24"
+					/>
+				</div>
+			</div>
 			<a
 				class="btn btn-outline"
 				href={`https://hub.api.eotdl.com/services/eoxhub-gateway/eotdl/notebook-view/notebooks/07_training_template.ipynb`}
@@ -28,7 +57,7 @@
 					class="underline"
 					href="https://hub.api.eotdl.com/services/eoxhub-gateway/eotdl/notebook-view/notebooks/07_training_template.ipynb"
 					target="_blank">here</a
-				>
+				>.
 			</p>
 		</form>
 		<div class="modal-action">
