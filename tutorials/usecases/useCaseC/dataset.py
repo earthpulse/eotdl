c3f212732ae7d08a12e617b2f1beb52ffefd2459
juansensio <sensio.juan@gmail.com>
2025-02-07 15:30:23 +0100

    use case c

---
+++ tutorials/usecases/useCaseC/dataset.py
@@ -0,0 +1,109 @@
+# script to prepare the dataset (download Satellogic + S1/S2 images)
+#  SH API rate limit: 1200 requests/minute
+REQUEST_LIMIT = 1200
+
+path = 'data/'
+dst_path = 'data/'
+
+
+SAMPLES_PER_ZONE= 100 # 69 zones
+TIME_BUFFER = 5 # days
+CLOUD_COVER_THRESHOLD = 0.0 # %
+WIDTH = 38
+HEIGHT = 38
+
+import geopandas as gpd
+
+print("Reading Satellogic Earthview items... ", end="", flush=True)
+gdf = gpd.read_parquet(path + 'satellogic-earthview-items.parquet')
+print("Done")
+
+zones = gdf['zone'].unique()
+
+import json
+from pathlib import Path
+import requests
+
+def download_images(json_path, centroid, date):
+	# sentinel
+	name = json_path.split('/')[-1].replace('_metadata.json', '_S2L2A')
+	dst_path_sentinel = dst_path + "/tifs/sentinel2/" + name + '.tif'
+	if not Path(dst_path_sentinel).exists():
+		custom_bbox = bbox_from_centroid(x=centroid.y, y=centroid.x, pixel_size=10, width=WIDTH, height=HEIGHT)
+		try:
+			download_sentinel_imagery(dst_path + "/tifs/sentinel2", date, custom_bbox, "sentinel-2-l2a", name=name)
+			Path(str(dst_path_sentinel).replace('.tif', '.json')).unlink(missing_ok=True)
+		except Exception as e:
+			print(f"Error downloading sentinel imagery: {e}")
+			return 0
+	# satellogic
+	with open(json_path, 'r') as f:
+		metadata = json.load(f)
+	url = metadata['assets']['analytic']['href']
+	output_path = Path(dst_path + "tifs/satellogic") / url.split('/')[-1]
+	if not output_path.exists():
+		output_path.parent.mkdir(parents=True, exist_ok=True)
+		response = requests.get(url, stream=True)
+		response.raise_for_status()
+		with open(output_path, 'wb') as f:
+			for chunk in response.iter_content(chunk_size=8192):
+				f.write(chunk)
+	return 1
+
+def search_matches(args):
+	date, bb, json_path, centroid = args
+	dates = [(date - timedelta(days=TIME_BUFFER/2)).strftime('%Y-%m-%d'), (date + timedelta(days=TIME_BUFFER/2)).strftime('%Y-%m-%d')]
+	bb = [bb[0], bb[1], bb[2], bb[3]]
+	results = list(search_sentinel_imagery(dates, bb, 'sentinel-2-l2a'))
+	if len(results) > 0:
+		# filter by cloud cover
+		results_filtered = [r for r in results if r['properties']['eo:cloud_cover'] <= CLOUD_COVER_THRESHOLD]
+		if len(results_filtered) > 0:
+			# Find closest match by date
+			closest_match = min(results_filtered, key=lambda x: abs(datetime.fromisoformat(x['properties']['datetime'].replace('Z','')) - date))
+			return download_images(json_path, centroid, closest_match['properties']['datetime'])
+		return 0
+	return 0
+
+# def rate_limited_request(func, *args, **kwargs):
+# 	with request_lock:
+# 		current_time = time.time()
+# 		# Remove requests older than 1 minute
+# 		while request_times and request_times[0] < current_time - 60:
+# 			request_times.pop(0)
+# 		# Wait if we've hit the limit
+# 		if len(request_times) >= REQUEST_LIMIT:
+# 			sleep_time = request_times[0] - (current_time - 60)
+# 			if sleep_time > 0:
+# 				time.sleep(sleep_time)
+# 		# Make the request
+# 		result = func(*args, **kwargs)
+# 		request_times.append(time.time())
+# 		return result
+		
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
+from eotdl.access import search_sentinel_imagery, download_sentinel_imagery
+from datetime import datetime, timedelta
+from eotdl.tools import bbox_from_centroid
+
+num_cores = multiprocessing.cpu_count()
+for z, zone in enumerate(zones):
+	print("Zone:", zone, f"({z+1}/{len(zones)})")
+	zone_gdf = gdf[gdf['zone'] == zone]
+	sample = zone_gdf.sample(SAMPLES_PER_ZONE, random_state=2025)
+	args = [(item.date, item.geometry.bounds, item.json_path, item.geometry.centroid) for _, item in sample.iterrows()]
+	with ProcessPoolExecutor(max_workers=num_cores) as pool:
+		with tqdm(total=len(args)) as progress:
+			futures = []
+			for arg in args:
+				future = pool.submit(search_matches, arg) 
+				future.add_done_callback(lambda p: progress.update())
+				futures.append(future)
+			results = []
+			for future in futures:
+				result = future.result()
+				results.append(result)
+	print("Found", sum(results), "matches")
+
