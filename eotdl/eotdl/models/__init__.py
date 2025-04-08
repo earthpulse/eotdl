075a0c51c6f681497e51dc156c0569f8fd59fc91
juan <juan@MacBook-Pro-de-juan.local>
2025-04-08 13:17:13 +0200

    update

--- eotdl/eotdl/models/__init__.py
+++ eotdl/eotdl/models/__init__.py
@@ -1,3 +1,3 @@
 from .retrieve import retrieve_models, retrieve_model, retrieve_model_files
-from .ingest import ingest_model
+from .ingest import ingest_model, ingest_virtual_model, ingest_model_catalog
 from .stage import stage_model, stage_model_file
