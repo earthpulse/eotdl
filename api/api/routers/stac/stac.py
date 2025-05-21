595895c3af4edc6de9411d35a62e6a8321fd29f3
derekvdven <derekvdven@gmail.com>
2025-05-21 12:01:33 +0200

    merge and fix stac landing page and remove tests

--- api/api/routers/stac/stac.py
+++ api/api/routers/stac/stac.py
@@ -8,53 +8,6 @@ from ...src.usecases.stac import retrieve_stac_collections, retrieve_stac_collec
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
-
-# here the core conformance class needs to be implemented
-@router.get("")
-def stac():
-
-    core_response = {
-        "stac_version": "1.0.0",
-        "id": "eotdl-stac-api",
-        "title": "EOTDL STAC API",
-        "description": "EOTDSL STAC API Landing Page",
-        "type": "Catalog",
-        "conformsTo": [
-            "https://api.stacspec.org/v1.0.0/core",
-            "https://api.stacspec.org/v1.0.0/item-search"
-            "https://api.stacspec.org/v1.0.0/collections"
-        ],
-        "links": [
-            {
-            "rel": "self",
-            "type": "application/json",
-            "href": "https://api.eotdl.com"
-            },
-            {
-            "rel": "root",
-            "type": "application/json",
-            "href": "https://api.eotdl.com"
-            },
-            {
-            "rel": "search",
-            "type": "application/json",
-            "href": "https://api.eotdl.com/search"
-            },
-            {
-            "rel": "collections",
-            "type": "application/json",
-            "href": "https://api.eotdl.com/collections"
-            }
-        ]
-        }
-
-
-    return core_response
-
-# there also needs to be a "/api" that returns a description of the service
-@router.get("/api")
-def api():
-    return "Endpoint include collections and search"
     
 @router.get("/collections")
 def collections():
