5cc2e2cb184ad83c5d1252c4126aacc96bc3e176
Juan Sensio <sensio.juan@gmail.com>
2025-06-02 09:45:41 +0200

    update

--- ui/src/routes/Banner.svelte
+++ ui/src/routes/Banner.svelte
@@ -1,6 +1,9 @@
-<div class="w-full h-10 bg-[#4abfa7] flex items-center justify-center">
-    <p class="text-white">
-        <b>Join us</b> at the Living Planet Symposium 2025 (23—27 June 2025) in
-        Vienna 🌍. Learn more <a href="/blog/LPS25" class="underline">here</a>.
-    </p>
+<div class="w-full bg-[#4abfa7] flex items-center justify-center">
+    <div class="max-w-7xl p-4 text-center">
+        <p class="text-white">
+            <b>Join us</b> at the Living Planet Symposium 2025 (23—27 June 2025)
+            in Vienna 🌍. Learn more
+            <a href="/blog/LPS25" class="underline">here</a>.
+        </p>
+    </div>
 </div>
