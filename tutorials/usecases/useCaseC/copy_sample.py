22b9db9baf301fc2547d3f6952f845a2b87fac5b
Ben Stahl <bes227@lehigh.edu>
2025-06-10 15:35:27 +0200

    num sample var

--- tutorials/usecases/useCaseC/copy_sample.py
+++ tutorials/usecases/useCaseC/copy_sample.py
@@ -5,13 +5,14 @@ import shutil
 
 data_path = '/fastdata/Satellogic/data/tifs/satellogic/'
 
+NUM_SAMPLES = 100
 
 if __name__ == "__main__":
     # Get all files in the directory
     all_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f))]
 
     # Sample 100 files (if there are at least 100 files)
-    sample_size = min(100, len(all_files))
+    sample_size = min(NUM_SAMPLES, len(all_files))
     sampled_files = random.sample(all_files, sample_size)
 
     # Print sampled file names
