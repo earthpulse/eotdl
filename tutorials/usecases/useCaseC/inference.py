d9920bab15f47243f7cebb037756fa93e54e0472
juansensio <sensio.juan@gmail.com>
2025-06-16 16:33:50 +0200

    use cases

--- tutorials/usecases/useCaseC/inference.py
+++ tutorials/usecases/useCaseC/inference.py
@@ -36,7 +36,7 @@ def load_image(image, bands=(1,2,3), norm_values=4000):
 		image = src.read(bands)
 	return np.expand_dims((image / norm_values).clip(0, 1).astype(np.float32), axis=0)
 
-@app.post("/{model}")
+@app.post("/classification/{model}")
 async def classification(
 	request: Request,  
 	model: str, 
@@ -47,20 +47,17 @@ async def classification(
 	return outputs.tolist()
 
 
-@app.post("/{model}")
+@app.post("/segmentation/{model}")
 async def segmentation(
 	request: Request,  
 	model: str, 
 	image: UploadFile = File(...), 
 ):
-    image = load_image(image)
-    outputs = inference(model, image)
-    if outputs.ndim == 3:  # get first band
-        outputs = outputs[0]
-    # outputs = resize(outputs, model_wrapper.original_size, preserve_range=True)
-    # outputs = outputs.astype(np.uint8)
-    img = Image.fromarray(outputs, mode="F")  # only returns binary mask
-    buf = io.BytesIO()
-    img.save(buf, "tiff")
-    buf.seek(0)
-    return StreamingResponse(buf, media_type="image/tiff") 
+	image = load_image(image)
+	outputs = inference(model, image)
+	outputs = np.argmax(outputs[0], axis=0) + 1
+	img = Image.fromarray(outputs.astype(np.uint16))  # Use uint16 to preserve integer values
+	buf = io.BytesIO()
+	img.save(buf, "tiff")
+	buf.seek(0)
+	return StreamingResponse(buf, media_type="image/tiff")
