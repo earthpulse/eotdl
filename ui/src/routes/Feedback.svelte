2c5cabac2aecbca90dc394bc268573551d3ff523
juansensio <sensio.juan@gmail.com>
2025-06-20 14:36:00 +0200

    update

---
+++ ui/src/routes/Feedback.svelte
@@ -0,0 +1,33 @@
+<div
+    id="feedback"
+    class="w-full flex flex-col items-center py-24"
+    style="background: url('backgrounds/Group-1190.png') center center/cover;"
+>
+    <div
+        class="gap-12 text-white flex flex-col md:flex-row items-center px-4 justify-center h-full max-w-3xl mx-auto"
+    >
+        <div class="flex flex-col gap-4">
+            <h1 class="text-4xl font-bold">Got feedback?</h1>
+            <h1 class="text-4xl font-bold">We'd love to hear it!</h1>
+            <p class="text-md">
+                EOTDL is a community dirven initiative and as such we would love
+                to hear from you
+            </p>
+            <p class="text-md">
+                What do you like about EOTDL? Whhat features are you missing?
+                What would you like tos ee improve?
+            </p>
+            <p class="text-md">
+                Follow the QR code or hyperlink on the irght, complete the
+                survey and let us know your feedback. Your ideas and voice
+                matter.
+            </p>
+        </div>
+        <div class="flex flex-col items-center gap-4">
+            <a href="http://subscribepage.io/YcTU97" target="_blank">
+                <img src="QR.png" alt="QR Code" />
+            </a>
+            <p class="text-center font-bold">Click or scan the QR code</p>
+        </div>
+    </div>
+</div>
