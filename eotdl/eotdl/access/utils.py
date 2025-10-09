e962f88f60d9718679b36e0648bdd9b1eaed7e9c
juan <it@earthpulse.es>
2025-10-09 12:47:13 +0200

    usecase A pipeline

--- eotdl/eotdl/access/utils.py
+++ eotdl/eotdl/access/utils.py
@@ -1,10 +1,9 @@
-from .matches import find_sentinel_matches_by_centroid
-from .download import download_sentinel_imagery
-
 def _find_matches(args):
+    from .matches import find_sentinel_matches_by_centroid
     ix, centroid, date, time_buffer, width, height, collection_id = args
     return (ix, find_sentinel_matches_by_centroid(centroid, date, time_buffer, width, height, collection_id))
 
 def _download_sentinel_imagery(args):
+    from .download import download_sentinel_imagery
     dir, date, bb, col, name = args
     download_sentinel_imagery(dir, date, bb, col, name)
