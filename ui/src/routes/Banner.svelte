7021068c03f7498d8f923c6bb824bf9d803b7645
earthpulse <sensio.juan@gmail.com>
2025-11-21 13:09:37 +0100

    update

--- ui/src/routes/Banner.svelte
+++ ui/src/routes/Banner.svelte
@@ -1,14 +1,14 @@
 <div class="w-full bg-[#4abfa7] flex items-center justify-center">
     <div class="max-w-7xl p-4 text-center">
         <p class="text-white">
-            <b>EOTDL</b> is now available in
+            🚀 Join us for the
             <a
-                href="https://radiantearth.github.io/stac-browser/#/external/api.eotdl.com/stac?.language=en"
+                href="https://eu01web.zoom.us/webinar/register/WN_gCYFSn5NScOnhor_lY34ZQ#/registration"
                 class="underline"
                 target="_blank"
-                rel="noopener noreferrer">STAC Browser</a
-            >! Explore datasets and models with a user-friendly catalog
-            interface.
+                rel="noopener noreferrer">EOTDL Community Workshop</a
+            >– Live Webinar 🗓 Wednesday, 26th | 17:00 CET | Free registration
+            required
         </p>
     </div>
 </div>
